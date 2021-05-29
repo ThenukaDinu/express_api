7fcc36969632505414ae27ecd24b85faa5718a9b
Thenukad Dinu <thenukadinu@gmail.com>
2021-05-29 15:29:40 +0530

    add CURD methods.... add CORS ADD json-parser middleware connected to monogo db extract configurations to external .env file

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const port = 3000
 const app = express();
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv/config')
 
-// Routes
+// Middleware
+app.use(cors())
+app.use(bodyParser.json())
+
+//Import Routes 
+const postRoutes = require('./routes/posts')
+
+app.use('/posts', postRoutes)
+
 app.get('/', (req, res) => {
     res.send('We are on Home')
 })
-app.get('/posts', (req, res) => {
-    res.send('We are on Posts')
-})
+
+// Connect To DB
+mongoose.connect(process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log('Connected To DB');
+    })
 
 // listening
-app.listen(port ,() => {
+app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 })
